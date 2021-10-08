3aa4af3a96db57b72ea98af3b4e93aebdc02e298
Ashish Patel <ashishp.nuku@gmail.com>
2021-10-08 16:04:43 +0530

    Innitial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value ,0));
+    title.value='';
+    link.value='';
+    return false;
+  }
+
+  removeArticle(article: Article): boolean {
+    console.log('Removing article title:' + article.title + ' and link:' + article.link);
+    this.articles = this.articles.filter((e) => {
+      if (e.title == article.title) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    } );
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+}
